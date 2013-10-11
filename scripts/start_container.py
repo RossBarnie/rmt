72c81e67780a9f7195a88dbdf741a62927ff917b
Ross Barnie <r.barnie@gmail.com>
2013-10-11 12:19:04 +0100

    added python code to test permissions

--- scripts/start_container.py
+++ scripts/start_container.py
@@ -1,10 +1,18 @@
 # Script to start an LXC container, assumes dependencies resolved
 # assuming linux platform
 
-#import os
+import os
 #import sys
+import stat
 
 # hard-coding test container for now
-#import errno
 
-print "permission granted"
+# need root access to start containers
+st = os.stat("./start_container.py")  # possibly heavy?
+if not bool(st.st_mode & stat.S_IXUSR):
+    print "permission denied, run as root"
+    exit(1)
+
+
+
+
