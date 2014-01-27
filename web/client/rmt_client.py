0494022a9a655a01b9b99e5ff3cea1a70a275025
Ross Barnie <r.barnie@gmail.com>
2014-01-27 16:42:15 +0000

    renamed for clarity and added content-type declarations

--- web/client/rmt_client.py
+++ web/client/rmt_client.py
@@ -14,6 +14,7 @@ class cpu:
     def GET(self):
         utilisation = psutil.cpu_times_percent(interval=1)
         dump = json.dumps(utilisation)
+        web.header('Content-Type', 'application/json')
         return dump
 
 
@@ -23,6 +24,7 @@ class ram:
         ram_total = psutil.virtual_memory().total
         ram_used = psutil.virtual_memory().used
         dict = {'ram_total': ram_total, 'ram_used': ram_used}
+        web.header('Content-Type', 'application/json')
         return json.dumps(dict)
 
 if __name__ == "__main__":
